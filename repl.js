a40fdd9412e0ebbf7da92196454f6232832a5140
Brian LeRoux <b@brian.io>
2017-11-19 16:45:52 -0800

    adds arc-repl

---
+++ repl.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+var data = require('.')
+var repl = require('repl')
+var chalk = require('chalk')
+var prompt = chalk.green('#!/data> ')
+var workflows = require('@architect/workflows')
+
+var db = workflows.sandbox.db.start(x=> {
+  var server = repl.start({prompt})
+  server.context.data = data()
+  server.on('exit', x=> db.close())
+})
