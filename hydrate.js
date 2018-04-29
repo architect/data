7785e8f18d07974da991a6763f9b2d45e9fa4e25
Brian LeRoux <b@brian.io>
2018-04-29 15:41:19 -0500

    fix path

--- hydrate.js
+++ hydrate.js
@@ -5,7 +5,7 @@ var exists = require('file-exists')
 var init = require('./_init')
   
 // check for node_modules/@architect/shared/.arc
-var arcPath = path.join(__dirname, 'shared', '.arc')
+var arcPath = path.join(__dirname, '..', 'shared', '.arc')
 var notFound = !exists.sync(arcPath)
 if (notFound)
   throw ReferenceError('.arc file not found: ' + arcPath)
