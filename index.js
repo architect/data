5316d7ce43bd2003a200af9b3bd4f18f7d22faa7
Ryan Block <rblock@gmail.com>
2019-01-15 10:26:30 -0800

    Comments / style

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ var init = require('./src/_init')
 // path to the .arc for hydration
 var arcPath
 
-let arc4sandbox = path.join(process.cwd(), 'node_modules', '@architect', 'shared', '.arc'),
-  arcInCurrentDir = path.join(process.cwd(), '.arc'),
-  arcInSharedDir = path.join(__dirname, '..', 'shared', '.arc')
+let arcDefault = path.join(process.cwd(), 'node_modules', '@architect', 'shared', '.arc')
+let arcInCurrentDir = path.join(process.cwd(), '.arc')
+let arcInSharedDir = path.join(__dirname, '..', 'shared', '.arc')
 
-// Arc 4
-if (exists(arc4sandbox)) arcPath = arc4sandbox
+if (exists(arcDefault)) {
+  // Arc default path (used in Arc 4 as well as ARC_LOCAL)
+  arcPath = arcDefault
+}
 else if (exists(arcInCurrentDir)) {
-  // implicitly if .arc is in the cwd use that (used in arc 3)
+  // If .arc is in the cwd, use that (used in Arc 3)
   arcPath = arcInCurrentDir
-} else if (exists(arcInSharedDir)) {
-  // otherwise we are: testing, staging or in production and loading from within node_modules
-  // Eg, node_modules/@architect/shared/.arc
+}
+else if (exists(arcInSharedDir)) {
+  // Otherwise we are: testing, staging, or in production and loading from within node_modules
+  // Eg, ./node_modules/@architect/shared/.arc  
   arcPath = arcInSharedDir
-} else {
+}
+else {
   throw ReferenceError('.arc file not found: ' + arcPath)
 }
 
